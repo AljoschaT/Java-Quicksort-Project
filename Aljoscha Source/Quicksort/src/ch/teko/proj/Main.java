d057be7a52508ee4a02ebf236b0d1fd86b24b537
AljoschaT <aljoscha.troester@edu.teko.ch>
2022-03-20 12:53:15 +0100

    init commit

--- Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
+++ Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
@@ -6,14 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-	Quicksort quick = new Quicksort();
+        Quicksort quick = new Quicksort();
 
-    int [] sortiereMich = {89, 81, 99, 26, 80, 59, 60, 20, 15, 47, 70, 90, 12, 62, 68, 53, 46, 34, 17, 67, 12, 18, 57, 100, 30};
-    int arraysLänge = sortiereMich.length - 1;
+        int [] sortiereMich = {32, 235, 107, 106, 139, 52, 9, 24, 219, 192, 146, 191, 41, 71, 20, 127, 7, 167, 141, 5};
+
+        int arraysLaenge = sortiereMich.length - 1;  //Um nicht immer die Arraylänge nachtragen zu müssen, wird hier die passende Indexzahl ermittelt um es später einfügen zu können
 
 
-    System.out.println(Arrays.toString(sortiereMich));
-    quick.quicksort(sortiereMich, 0, arraysLänge);
-    System.out.println(Arrays.toString(sortiereMich));
+        System.out.println(Arrays.toString(sortiereMich));
+        quick.quicksort(sortiereMich, 0, arraysLaenge);
+        System.out.println(Arrays.toString(sortiereMich));
     }
 }
--- Aljoscha Source/Quicksort/src/ch/teko/proj/Quicksort.java
+++ Aljoscha Source/Quicksort/src/ch/teko/proj/Quicksort.java
@@ -3,6 +3,13 @@ package ch.teko.proj;
 public class Quicksort {
 
     public int aufteilung(int liste[], final int ersteselement, final int letzteselement) {
+
+        /*
+         * Der Pivot wird ermittelt, in dem der Index ganz links (meist 0)
+         * und der Index ganz rechts addiert und danach durch zwei geteilt werden
+         * So wird eine Median Pivot ermittelt
+         * Sollte eine ungerade Array genommen werden, wird die Zahl abgerundet
+         */
         int pivot = liste[(ersteselement + letzteselement) /2];
 
         int linkePosition = ersteselement;
