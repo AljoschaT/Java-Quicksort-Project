61cc0d440b472582e88f5a2590aab49104079927
AljoschaT <aljoscha.troester.edu.teko.ch>
2022-03-28 16:03:28 +0200

    Weitere Kommentare hinzugefühgt

--- Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
+++ Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
@@ -8,17 +8,20 @@ public class Main {
 
         Quicksort quick = new Quicksort();
 
+        
+        
+        // Deklaration des Array's
         int [] sortiereMich = {32, 235, 107, 106, 139, 52, 9, 24, 219, 192, 146, 191, 41, 71, 20, 127, 7, 167, 141, 5};
 
         /*
-        *Um nicht immer die Arraylänge nachtragen zu müssen,
-        *wird hier die passende Indexzahl ermittelt um es später einfügen zu können
+        * Um nicht immer die Arraylänge nachtragen zu müssen,
+        * wird hier die passende Indexzahl ermittelt um es später einfügen zu können
         */
         int arraysLaenge = sortiereMich.length - 1;  
 
 
-        System.out.println(Arrays.toString(sortiereMich));
+        System.out.println(Arrays.toString(sortiereMich)); //Gibt das unsortiere Array aus
         quick.quicksort(sortiereMich, 0, arraysLaenge);
-        System.out.println(Arrays.toString(sortiereMich));
+        System.out.println(Arrays.toString(sortiereMich)); //Gibt das sortiere Array aus
     }
 }
