954cd68bcda19f639650b0b74c5269ced96ca154
AljoschaT <aljoscha.troester.edu.teko.ch>
2022-03-28 14:12:20 +0200

    Beschreibung anpassen

--- Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
+++ Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
@@ -10,7 +10,11 @@ public class Main {
 
         int [] sortiereMich = {32, 235, 107, 106, 139, 52, 9, 24, 219, 192, 146, 191, 41, 71, 20, 127, 7, 167, 141, 5};
 
-        int arraysLaenge = sortiereMich.length - 1;  //Um nicht immer die Arraylänge nachtragen zu müssen, wird hier die passende Indexzahl ermittelt um es später einfügen zu können
+        /*
+        *Um nicht immer die Arraylänge nachtragen zu müssen,
+        *wird hier die passende Indexzahl ermittelt um es später einfügen zu können
+        */
+        int arraysLaenge = sortiereMich.length - 1;  
 
 
         System.out.println(Arrays.toString(sortiereMich));
