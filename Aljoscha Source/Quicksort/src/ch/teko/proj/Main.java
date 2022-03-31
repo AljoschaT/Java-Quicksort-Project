317d61233e15fa5df5f441b170a3cc18c03c10d8
AljoschaT <aljoscha.troester@edu.teko.ch>
2022-03-31 20:04:35 +0200

    init commit

--- Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
+++ Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
@@ -2,33 +2,26 @@ package ch.teko.proj;
 
 import java.util.Arrays;
 
-
-
 public class Main {
 
     public static void main(String[] args) {
 
         Quicksort quick = new Quicksort();
 
-        
-        
-        // Deklaration des Array's
-        int [] sortiereMich = new int[100];
 
-        for (int i = 0; i < sortiereMich.length; i++) {
-            sortiereMich[i] = (int) (Math.random()*100); // Jedes Element erhält eine Random Zahl.
-        }
+
+        // Deklaration des Array's 
+        int [] sortiereMich = {32, 235, 107, 106, 139, 52, 9, 24, 219, 192, 146, 191, 41, 71, 20, 127, 7, 167, 141, 5};
 
         /*
-        * Um nicht immer die Arraylänge nachtragen zu müssen,
-        * wird hier die passende Indexzahl ermittelt um es später einfügen zu können
-        */
+         * Um nicht immer die Arraylänge nachtragen zu müssen,
+         * wird hier die passende Indexzahl ermittelt um es später einfügen zu können
+         */
         int arraysLaenge = sortiereMich.length - 1;
 
 
-
-        System.out.println("Unsortiert: " + Arrays.toString(sortiereMich)); //Gibt das unsortiere Array aus
+        System.out.println(Arrays.toString(sortiereMich)); //Gibt das unsortiere Array aus 
         quick.quicksort(sortiereMich, 0, arraysLaenge);
-        System.out.println("Sortiert: " + Arrays.toString(sortiereMich)); //Gibt das sortiere Array aus
+        System.out.println(Arrays.toString(sortiereMich)); //Gibt das sortiere Array aus 
     }
-}
+} 
