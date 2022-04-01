f58aa65ea09ceafa54ed9d8f31ca83ad6869f3b4
AljoschaT <aljoscha.troester.edu.teko.ch>
2022-04-01 10:52:20 +0200

    Codesäuberung

--- Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
+++ Aljoscha Source/Quicksort/src/ch/teko/proj/Main.java
@@ -8,16 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
         Quicksort quick = new Quicksort();
-
-
                   
         // Deklaration des Random Array's 
         int [] sortiereMich = new Random().ints().limit(10).toArray();
 
-
         // Deklaration des Fixen Array's 
        // int [] sortiereMich = {32, 235, 107, 106, 139, 52, 9, 24, 219, 192, 146, 191, 41, 71, 20, 127, 7, 167, 141, 5};
-
 
         /*
          * Um nicht immer die Arraylänge nachtragen zu müssen,
@@ -25,14 +21,9 @@ public class Main {
          */
         int arraysLaenge = sortiereMich.length - 1;
 
-
         System.out.println(Arrays.toString(sortiereMich)); //Gibt das unsortiere Array aus 
         quick.quicksort(sortiereMich, 0, arraysLaenge);
-
         System.out.println(Arrays.toString(sortiereMich)); //Gibt das sortiere Array aus
-
-
-        System.out.println(Arrays.toString(sortiereMich)); //Gibt das sortiere Array aus 
 
     }
 } 
